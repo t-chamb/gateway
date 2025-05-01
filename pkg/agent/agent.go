53ff7a24b814c5504049a6093a765dac07265769
Sergei Lukianov <me@slukjanov.name>
2025-05-01 00:35:28 -0700

    feat: add basic dataplane config generation
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/agent/agent.go
+++ pkg/agent/agent.go
@@ -7,7 +7,9 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"maps"
 	"os"
+	"slices"
 	"strings"
 	"time"
 
@@ -171,7 +173,14 @@ func handleAgent(ctx context.Context, cfg *meta.AgentConfig, ag *gwintapi.Gatewa
 		if resp.Generation != uint64(ag.Generation) { //nolint:gosec // TODO fix proto
 			slog.Info("Dataplane config needs to be updated", "current", resp.Generation, "new", ag.Generation)
 
-			resp, err := client.UpdateConfig(ctx, &dataplane.UpdateConfigRequest{})
+			gwCfg, err := buildDataplaneConfig(ag)
+			if err != nil {
+				return fmt.Errorf("building dataplane config: %w", err)
+			}
+
+			resp, err := client.UpdateConfig(ctx, &dataplane.UpdateConfigRequest{
+				Config: gwCfg,
+			})
 			if err != nil {
 				return fmt.Errorf("updating config: %w", err)
 			}
@@ -185,4 +194,114 @@ func handleAgent(ctx context.Context, cfg *meta.AgentConfig, ag *gwintapi.Gatewa
 	}
 
 	return nil
+}
+
+func buildDataplaneConfig(ag *gwintapi.GatewayAgent) (*dataplane.GatewayConfig, error) {
+	cfg := &dataplane.GatewayConfig{
+		Generation: uint64(ag.Generation), //nolint:gosec // TODO fix proto
+		Device: &dataplane.Device{
+			Driver:   dataplane.PacketDriver_KERNEL,
+			Hostname: ag.Name,
+			Loglevel: dataplane.LogLevel_DEBUG,
+		},
+		Underlay: &dataplane.Underlay{ // TODO replace with actual generated config
+			Vrf: []*dataplane.VRF{
+				{
+					Name: "default",
+					Interfaces: []*dataplane.Interface{
+						{
+							Name:   "eth0",
+							Ipaddr: "10.0.0.1/32",
+							Type:   dataplane.IfType_IF_TYPE_ETHERNET,
+							Role:   dataplane.IfRole_IF_ROLE_FABRIC,
+						},
+						{
+							Name:   "eth1",
+							Ipaddr: "10.0.0.1/32",
+							Type:   dataplane.IfType_IF_TYPE_ETHERNET,
+							Role:   dataplane.IfRole_IF_ROLE_EXTERNAL,
+						},
+					},
+				},
+			},
+		},
+		Overlay: &dataplane.Overlay{},
+	}
+
+	// TODO do we need to bother with sorting?
+
+	for _, vpcName := range slices.Sorted(maps.Keys(ag.Spec.VPCs)) {
+		vpc := ag.Spec.VPCs[vpcName]
+		cfg.Overlay.Vpcs = append(cfg.Overlay.Vpcs, &dataplane.VPC{
+			Name: vpcName,
+			Id:   vpc.InternalID,
+			Vni:  vpc.VNI,
+		})
+	}
+
+	for _, peeringName := range slices.Sorted(maps.Keys(ag.Spec.Peerings)) {
+		peering := ag.Spec.Peerings[peeringName]
+		p := &dataplane.VpcPeering{
+			Name: peeringName,
+			For:  []*dataplane.PeeringEntryFor{},
+		}
+
+		for _, vpcName := range slices.Sorted(maps.Keys(peering.Peering)) {
+			vpc := peering.Peering[vpcName]
+			exposes := []*dataplane.Expose{}
+
+			for _, expose := range vpc.Expose {
+				ips := []*dataplane.PeeringIPs{}
+				as := []*dataplane.PeeringAs{}
+
+				for _, ipEntry := range expose.IPs {
+					// TODO validate
+					switch {
+					case ipEntry.CIDR != "":
+						ips = append(ips, &dataplane.PeeringIPs{
+							Rule: &dataplane.PeeringIPs_Cidr{Cidr: ipEntry.CIDR},
+						})
+					case ipEntry.Not != "":
+						ips = append(ips, &dataplane.PeeringIPs{
+							Rule: &dataplane.PeeringIPs_Not{Not: ipEntry.Not},
+						})
+					case ipEntry.VPCSubnet != "":
+						return nil, fmt.Errorf("vpcSubnet not supported yet") //nolint:goerr113 // TODO
+					default:
+						return nil, fmt.Errorf("invalid IP entry in peering %s / vpc %s: %v", peeringName, vpcName, ipEntry) //nolint:goerr113
+					}
+				}
+
+				for _, asEntry := range expose.As {
+					// TODO validate
+					switch {
+					case asEntry.CIDR != "":
+						as = append(as, &dataplane.PeeringAs{
+							Rule: &dataplane.PeeringAs_Cidr{Cidr: asEntry.CIDR},
+						})
+					case asEntry.Not != "":
+						as = append(as, &dataplane.PeeringAs{
+							Rule: &dataplane.PeeringAs_Not{Not: asEntry.Not},
+						})
+					default:
+						return nil, fmt.Errorf("invalid IP entry in peering %s / vpc %s: %v", peeringName, vpcName, asEntry) //nolint:goerr113
+					}
+				}
+
+				exposes = append(exposes, &dataplane.Expose{
+					Ips: ips,
+					As:  as,
+				})
+			}
+
+			p.For = append(p.For, &dataplane.PeeringEntryFor{
+				Vpc:    vpcName,
+				Expose: exposes,
+			})
+		}
+
+		cfg.Overlay.Peerings = append(cfg.Overlay.Peerings, p)
+	}
+
+	return cfg, nil
 }
