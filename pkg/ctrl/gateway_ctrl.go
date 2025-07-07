25719d01e01d02df070c2517295d3c1de2e4a4a6
Sergei Lukianov <me@slukjanov.name>
2025-07-07 12:06:46 -0700

    chore: pass metrics addr to the dataplane container
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/ctrl/gateway_ctrl.go
+++ pkg/ctrl/gateway_ctrl.go
@@ -465,7 +465,7 @@ func (r *GatewayReconciler) deployGateway(ctx context.Context, gw *gwapi.Gateway
 									"--cli-sock-path", filepath.Join(dataplaneRunMountPath, "cli.sock"),
 									"--cpi-sock-path", filepath.Join(frrRunMountPath, cpiSocket),
 									"--frr-agent-path", filepath.Join(frrRunMountPath, frrAgentSocket),
-									"--metrics-port", fmt.Sprintf("%d", r.cfg.DataplaneMetricsPort),
+									"--metrics-address", fmt.Sprintf("127.0.0.1:%d", r.cfg.DataplaneMetricsPort),
 								}, ifaceFlags...),
 								SecurityContext: &corev1.SecurityContext{
 									Privileged: ptr.To(true),
